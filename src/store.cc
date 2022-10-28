036cd4a72db7dfa935c0bab782033e84765e2281
Samina Mulani <samina00@gmail.com>
2022-10-28 00:27:46 -0400

    Boilerplate (somewhat)
    Copied some code from the gRPC tutorial. Haven't built and tested this.

--- src/store.cc
+++ src/store.cc
@@ -1,15 +1,57 @@
 #include "threadpool.h"
 
 #include <iostream>
+#include <grpc++/grpc++.h>
+#include "store.grpc.pb.h"
+#include "vendor.grpc.pb.h"
 
-using namespace std;
+using grpc::Server;
+using grpc::ServerAsyncResponseWriter;
+using grpc::ServerBuilder;
+using grpc::ServerCompletionQueue;
+using grpc::ServerContext;
+using grpc::Status;
 
-class store { 
+// not sure if we need these
+using vendor::BidQuery;
+using vendor::BidReply;
+using vendor::Vendor;
+using store::ProductQuery;
+using store::ProductReply;
+using store::ProductInfo;
 
+class store final : public Store::AsyncService{ 
+	public:
+	~store() {
+		server_->Shutdown();
+		// Always shutdown the completion queue after the server.
+		cq_->Shutdown();
+	}
+	
+	void Run() {
+    std::string server_address("0.0.0.0:50051");
 
+    ServerBuilder builder;
+    // Listen on the given address without any authentication mechanism.
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    // Register "service_" as the instance through which we'll communicate with
+    // clients. In this case it corresponds to an *asynchronous* service.
+    builder.RegisterService(&service_);
+    // Get hold of the completion queue used for the asynchronous communication
+    // with the gRPC runtime.
+    cq_ = builder.AddCompletionQueue();
+    // Finally assemble the server.
+    server_ = builder.BuildAndStart();
+    std::cout << "Server listening on " << server_address << std::endl;
+
+    // Proceed to the server's main loop.
+    HandleRpcs();
+  }
 };
 
 int main(int argc, char** argv) {
+	ServerImpl server;
+  	server.Run();
 	std::cout << "I 'm not ready yet!" << std::endl;
 	return EXIT_SUCCESS;
 }
