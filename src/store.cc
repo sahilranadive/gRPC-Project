71a60e1ce55e5054b35278544241fe0002c6228b
Samina Mulani <samina00@gmail.com>
2022-10-28 01:52:14 -0400

    Boilerplate pt 2
    Copied more stuff. This one compiles

--- src/store.cc
+++ src/store.cc
@@ -19,10 +19,11 @@ using vendor::Vendor;
 using store::ProductQuery;
 using store::ProductReply;
 using store::ProductInfo;
+using store::Store;
 
-class store final : public Store::AsyncService{ 
+class StoreImpl final : public Store::AsyncService{ 
 	public:
-	~store() {
+	~StoreImpl() {
 		server_->Shutdown();
 		// Always shutdown the completion queue after the server.
 		cq_->Shutdown();
@@ -47,10 +48,102 @@ class store final : public Store::AsyncService{
     // Proceed to the server's main loop.
     HandleRpcs();
   }
+
+  private:
+  // Class encompasing the state and logic needed to serve a request.
+  class CallData {
+   public:
+    // Take in the "service" instance (in this case representing an asynchronous
+    // server) and the completion queue "cq" used for asynchronous communication
+    // with the gRPC runtime.
+    CallData(Store::AsyncService* service, ServerCompletionQueue* cq)
+        : service_(service), cq_(cq), responder_(&ctx_), status_(CREATE) {
+      // Invoke the serving logic right away.
+      Proceed();
+    }
+
+    void Proceed() {
+      if (status_ == CREATE) {
+        // Make this instance progress to the PROCESS state.
+        status_ = PROCESS;
+
+        // As part of the initial CREATE state, we *request* that the system
+        // start processing SayHello requests. In this request, "this" acts are
+        // the tag uniquely identifying the request (so that different CallData
+        // instances can serve different requests concurrently), in this case
+        // the memory address of this CallData instance.
+        service_->RequestgetProducts(&ctx_, &request_, &responder_, cq_, cq_,
+                                  this);
+      } else if (status_ == PROCESS) {
+        // Spawn a new CallData instance to serve new clients while we process
+        // the one for this CallData. The instance will deallocate itself as
+        // part of its FINISH state.
+        new CallData(service_, cq_);
+
+        // The actual processing.
+        
+
+        // And we are done! Let the gRPC runtime know we've finished, using the
+        // memory address of this instance as the uniquely identifying tag for
+        // the event.
+        status_ = FINISH;
+        responder_.Finish(reply_, Status::OK, this);
+      } else {
+        GPR_ASSERT(status_ == FINISH);
+        // Once in the FINISH state, deallocate ourselves (CallData).
+        delete this;
+      }
+    }
+
+   private:
+    // The means of communication with the gRPC runtime for an asynchronous
+    // server.
+    Store::AsyncService* service_;
+    // The producer-consumer queue where for asynchronous server notifications.
+    ServerCompletionQueue* cq_;
+    // Context for the rpc, allowing to tweak aspects of it such as the use
+    // of compression, authentication, as well as to send metadata back to the
+    // client.
+    ServerContext ctx_;
+
+    // What we get from the client.
+    ProductQuery request_;
+    // What we send back to the client.
+    ProductReply reply_;
+
+    // The means to get back to the client.
+    ServerAsyncResponseWriter<ProductReply> responder_;
+
+    // Let's implement a tiny state machine with the following states.
+    enum CallStatus { CREATE, PROCESS, FINISH };
+    CallStatus status_;  // The current serving state.
+  }; // END of Call Data Class
+
+  // This can be run in multiple threads if needed.
+  void HandleRpcs() {
+    // Spawn a new CallData instance to serve new clients.
+    new CallData(&service_, cq_.get());
+    void* tag;  // uniquely identifies a request.
+    bool ok;
+    while (true) {
+      // Block waiting to read the next event from the completion queue. The
+      // event is uniquely identified by its tag, which in this case is the
+      // memory address of a CallData instance.
+      // The return value of Next should always be checked. This return value
+      // tells us whether there is any kind of event or cq_ is shutting down.
+      GPR_ASSERT(cq_->Next(&tag, &ok));
+      GPR_ASSERT(ok);
+      static_cast<CallData*>(tag)->Proceed();
+    }
+  }
+
+  std::unique_ptr<ServerCompletionQueue> cq_;
+  Store::AsyncService service_;
+  std::unique_ptr<Server> server_;
 };
 
 int main(int argc, char** argv) {
-	ServerImpl server;
+	StoreImpl server;
   	server.Run();
 	std::cout << "I 'm not ready yet!" << std::endl;
 	return EXIT_SUCCESS;
